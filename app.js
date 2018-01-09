e70a4844ad85fa9413edde0a0b4e8d34bb0d2712
aditya1234 <kikchiemedia@gmail.com>
2018-01-09 13:27:13 +0700

    4

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/articles/add', function(req, res){
-    res.render('./partials/add_articles',{
+    res.render('./partials/add_article',{
         title: 'Add Articles'
     });
 });
@@ -67,12 +67,51 @@ app.post('/articles/add', function(req, res){
 
 app.get('/articles/:id', function(req, res){
     Article.findById(req.params.id, function(err, article){
-        res.render('./partials/article.hbs', {
+        res.render('./partials/article', {
             article: article
         });
     });
 });
 
+app.get('/articles/edit/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('./partials/edit_article',{
+            title: 'Edit',
+            article:article
+        });
+    });
+});
+
+app.post('/articles/edit/:id', function(req, res){
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id}
+
+    Article.update(query, article, function(err){
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+app.delete('/articles/:id', function(req, res){
+    let query = {_id: req.params.id}
+    Article.remove(query, function(err){
+        if(err){
+            console.log(err);
+        }
+        res.send('Success');
+    })
+})
+
 app.listen(3000, function(){
     console.log('Server started on port 3000');
 });
+
+
