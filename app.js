2396569f4edd30812b65ad9ab581772ce6441d71
aditya1234 <kikchiemedia@gmail.com>
2018-01-15 00:45:35 +0700

    6

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const expressValidator = require('express-validator');
 var flash = require('express-flash');
 
 let Article = require('./models/article');
-
 const app = express();
 var sessionStore = new session.MemoryStore;
 mongoose.connect('mongodb://localhost/blogger');
@@ -74,92 +73,13 @@ app.get('/', function(req, res){
         }
     });
 });
-
-app.get('/articles/add', function(req, res){
-    res.render('./partials/add_article',{
-        title: 'Add Articles'
-    });
-});
 app.all('/', function(req, res){
     req.flash('success', 'Article added')
     res.redirect('/');
 })
 
-app.post('/articles/add', function(req, res){
-    req.checkBody('title', 'Title is required').notEmpty();
-    req.checkBody('author', 'Author is required').notEmpty();
-    req.checkBody('body', 'Body is required').notEmpty();
-
-    let errors = req.validationErrors();
-    if(errors){
-        res.render('./partials/add_article',{
-            title: 'Add Article',
-            errors:errors
-        });
-    }else{
-        let article = new Article();
-        article.title = req.body.title;
-        article.author = req.body.author;
-        article.body = req.body.body;
-        article.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            req.flash('success', 'Article added');
-            res.redirect('/');
-        }
-    });
-    }
-
-    
-});
-
-app.get('/articles/:id', function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('./partials/article', {
-            article: article
-        });
-    });
-});
-
-app.get('/articles/edit/:id', function(req, res){
-    Article.findById(req.params.id, function(err, article){
-        res.render('./partials/edit_article',{
-            title: 'Edit',
-            article:article
-        });
-    });
-});
-
-app.post('/articles/edit/:id', function(req, res){
-    let article = {};
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-
-    let query = {_id:req.params.id}
-
-    Article.update(query, article, function(err){
-        if(err){
-            console.log(err);
-            return;
-        }else{
-            req.flash('success', 'Articles updated');
-            res.redirect('/');
-        }
-    });
-});
-
-app.delete('/articles/:id', function(req, res){
-    let query = {_id: req.params.id}
-    Article.remove(query, function(err){
-        if(err){
-            console.log(err);
-        }
-        req.flash('success','Article deleted');
-        res.send('Success');
-    })
-})
+let articles = require('./routes/articles');
+app.use('/articles', articles);
 
 app.listen(3000, function(){
     console.log('Server started on port 3000');
