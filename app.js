d877c854358b9fdc5ac3194fa000abe0df9af20a
aditya1234 <kikchiemedia@gmail.com>
2018-01-14 23:48:40 +0700

    5

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const expressHbs = require('express-handlebars');
 const mongoose = require('mongoose');
-const sessions = require('express-session');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-let Article = require('./models/article');
-const app = express();
+const expressValidator = require('express-validator');
+var flash = require('express-flash');
 
+let Article = require('./models/article');
+
+const app = express();
+var sessionStore = new session.MemoryStore;
 mongoose.connect('mongodb://localhost/blogger');
 let db = mongoose.connection;
 
@@ -22,14 +26,38 @@ db.on('error',()=>{
 app.engine('.hbs', expressHbs({defaultLayout:'layout', extname:'.hbs'}));
 app.set('view engine', '.hbs');
 
+app.use(flash());
 app.use(bodyParser.urlencoded({extended:app}));
 app.use(bodyParser.json());
-app.use(sessions({
+app.use(session({
     secret: 'I am secret',
-    resave: false,
-    saveUninitialized: true
+    resave: 'true',
+    saveUninitialized: true,
+    store: sessionStore
 }));
 
+app.use(function(req, res, next){
+    res.locals.sessionFlash = [];
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
@@ -37,9 +65,11 @@ app.get('/', function(req, res){
         if(err){
             console.log(err);
         }else{
+            //console.log(res.locals.sessionFlash);
             res.render('./partials/index',{
                 title:'Articles',
-                article: articles
+                article: articles,
+                expressFlash: req.flash('success')
             });
         }
     });
@@ -50,19 +80,38 @@ app.get('/articles/add', function(req, res){
         title: 'Add Articles'
     });
 });
+app.all('/', function(req, res){
+    req.flash('success', 'Article added')
+    res.redirect('/');
+})
 
 app.post('/articles/add', function(req, res){
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-    article.save(function(err){
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('author', 'Author is required').notEmpty();
+    req.checkBody('body', 'Body is required').notEmpty();
+
+    let errors = req.validationErrors();
+    if(errors){
+        res.render('./partials/add_article',{
+            title: 'Add Article',
+            errors:errors
+        });
+    }else{
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+        article.save(function(err){
         if(err){
             console.log(err);
         }else{
+            req.flash('success', 'Article added');
             res.redirect('/');
         }
     });
+    }
+
+    
 });
 
 app.get('/articles/:id', function(req, res){
@@ -95,6 +144,7 @@ app.post('/articles/edit/:id', function(req, res){
             console.log(err);
             return;
         }else{
+            req.flash('success', 'Articles updated');
             res.redirect('/');
         }
     });
@@ -106,6 +156,7 @@ app.delete('/articles/:id', function(req, res){
         if(err){
             console.log(err);
         }
+        req.flash('success','Article deleted');
         res.send('Success');
     })
 })
